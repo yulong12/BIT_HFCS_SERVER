162213fd914372a29a944d4095b940fe38f6d4db
yulong12 <312352372@qq.com>
2017-12-06 19:18:51 +0800

    去掉测试弹框

--- public/javascripts/page_switch.js
+++ public/javascripts/page_switch.js
@@ -5,22 +5,22 @@ function switch_to_user() {
 }
 
 function switch_to_hospital() {
-    alert("医院")
+  
     window.location.href="hospital";
 }
 function switch_to_civialAffirs() {
     window.location.href="civil_affairs";
 }
 function switch_to_userQuery() {
-    alert("个人信息查看");
+
     window.location.href="user_query";
 }
 function  switch_to_childregister() {
-    alert("child_register界面");
+
 window.location.href="child_register" ;
 }
 function  switch_to_childquery() {
-    alert("child_query界面");
+
     window.location.href="child_query" ;
 }
 function  switch_to_marryquery() {
