e71b3f98d8647ae75b7bb213aafaf9dd7f24641e
yulong12 <312352372@qq.com>
2017-12-19 13:58:15 +0800

    fished chile_register the databses

--- routes/child_register.js
+++ routes/child_register.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var options = require("./myhfc/org3Config");
+ var mysql=require('../db/MYSQLconnection');
 
 /* GET childregister listing. */
 router.get('/', function(req, res, next) {
@@ -26,6 +27,30 @@ router.post('/',function (req, res, next) {
     var mother_ID = req.body.mother_ID;
     var father_ID = req.body.father_ID;
     var hospital = req.body.hospital;
+    var mother_name=req.body.mother_name;
+    var mother_age=req.body.mother_age;
+    var father_name=req.body.father_name;
+    var father_age=req.body.father_age;
+    var child_name=req.body.child_name;
+    var child_weight=req.body.weight;
+    var child_health=req.body.health;
+
+    var mother_insert="insert into mother_table(mother_id,mother_name,mother_age)values("+"'"+mother_ID+"','"+mother_name+"',"+mother_age+");";
+    mysql.executeQuery(mother_insert,function () {
+
+    });
+    var father_insert="insert into father_table(father_id,father_name,father_age)values('"+father_ID+"','"+father_name+"',"+father_age+");";
+    var new_insert="insert into new_table(new_id,new_name,new_weight,new_health)values("+"'11111','"+child_name+"',"+child_weight+",'"+child_health+"');";
+    mysql.executeQuery(father_insert,function () {
+
+    });
+    mysql.executeQuery(new_insert,function () {
+
+    });
+    console.log("mother insert--------"+mother_insert);
+    console.log("father_insert-----"+father_insert);
+    console.log("new_insert-----"+new_insert);
+
     var request = {
         chaincodeId: options.chaincode_id,
         fcn: 'createBirth',
