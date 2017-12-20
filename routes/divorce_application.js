7383bfcace842dddc6a559c3b9753d7e46189336
HYSMN <2225830461@qq.com>
2017-12-20 13:51:50 +0800

    finished

--- routes/divorce_application.js
+++ routes/divorce_application.js
@@ -14,44 +14,44 @@ router.get('/', function(req, res, next) {
     }
 });
 
-var createFolder=function (folder) {
-    try{
-        fs.accessSync(folder);
-    }catch (e){
-        fs.mkdirSync(folder);
-    }
-};
-var uploaderFolder='e:/BIT/divorce_img';
-createFolder(uploaderFolder);
-var storage=multer.diskStorage({
-    destination:function (req, file, cb) {
-        cb(null,uploaderFolder);
-    },
-    filename:function (req, file, cb) {
-            req.files;
-        var new_name=file.fieldname + '-' + Date.now()+'.jpg';
-        // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
-        cb(null,new_name );
-    }
-});
-var upload = multer({ storage: storage });
-var cpUpload = upload.fields([{ name: 'men_img', maxCount: 1 }, { name: 'women_img', maxCount: 1 },{name:'divorce_img',maxCount:1}]);
-router.post('/',cpUpload,function (req, res, next) {
-
-
-    // var file=req.file;
-    console.log('文件类型：%s', req.files['men_img'].mimeType);
-    // console.log('原始文件名：%s', file.originalname);
-    // console.log('文件大小：%s', file.size);
-    // console.log('文件保存路径：%s', file.path);
-
-    // var str=file.path;
-    // console.log("--------------file.path"+str);
-    // var path=str.replace(/\\/g,"\\\\");
-    //
-    // console.log("------------------path"+path);
-
-    res.render('divorce_application');
-
-});
+// var createFolder=function (folder) {
+//     try{
+//         fs.accessSync(folder);
+//     }catch (e){
+//         fs.mkdirSync(folder);
+//     }
+// };
+// var uploaderFolder='e:/BIT/divorce_img';
+// createFolder(uploaderFolder);
+// var storage=multer.diskStorage({
+//     destination:function (req, file, cb) {
+//         cb(null,uploaderFolder);
+//     },
+//     filename:function (req, file, cb) {
+//             req.files;
+//         var new_name=file.fieldname + '-' + Date.now()+'.jpg';
+//         // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+//         cb(null,new_name );
+//     }
+// });
+// var upload = multer({ storage: storage });
+// var cpUpload = upload.fields([{ name: 'men_img', maxCount: 1 }, { name: 'women_img', maxCount: 1 },{name:'divorce_img',maxCount:1}]);
+// router.post('/',cpUpload,function (req, res, next) {
+//
+//
+//     // var file=req.file;
+//     console.log('文件类型：%s', req.files['men_img'].mimeType);
+//     // console.log('原始文件名：%s', file.originalname);
+//     // console.log('文件大小：%s', file.size);
+//     // console.log('文件保存路径：%s', file.path);
+//
+//     // var str=file.path;
+//     // console.log("--------------file.path"+str);
+//     // var path=str.replace(/\\/g,"\\\\");
+//     //
+//     // console.log("------------------path"+path);
+//
+//     res.render('divorce_application');
+//
+// });
 module.exports = router;
