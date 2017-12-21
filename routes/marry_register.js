50c4ab5e07099bf71fac363ee4f08ce962190b03
xuchenhao001 <xu.chenhao@hotmail.com>
2017-12-21 20:45:03 +0800

    finished marry register

--- routes/marry_register.js
+++ routes/marry_register.js
@@ -5,12 +5,45 @@ var mysql = require("../db/MYSQLconnection");
 
 /* GET marryregister listing. */
 router.get('/', function (req, res, next) {
+  var human_detail = {};
+
   if (req.session.user) {
     if (req.query.regist_num) {
       console.log(req.query.regist_num);
-      res.render('marry_register');
+      var query_marry = "select * from marry_check where check_id = \"" + req.query.regist_num + "\"";
+      mysql.executeQuery(query_marry, function (status, result) {
+        human_detail.husband_name = result.rows[0].husband_name;
+        human_detail.husband_id = result.rows[0].husband_id;
+        human_detail.husband_state = result.rows[0].husband_state;
+        human_detail.wife_name = result.rows[0].wife_name;
+        human_detail.wife_id = result.rows[0].wife_id;
+        human_detail.wife_state = result.rows[0].wife_state;
+        human_detail.marry_photo = result.rows[0].photo;
+
+        var query_husband = "select photo from human where id = \"" + human_detail.husband_id + "\"";
+        var query_wife = "select photo from human where id = \"" + human_detail.wife_id + "\"";
+
+        // search for wife and husband photo
+        mysql.executeQuery(query_husband, function (status, husband_detail) {
+          mysql.executeQuery(query_wife, function (status, wife_detail) {
+            console.log(husband_detail);
+            human_detail.husband_photo = husband_detail.rows[0].photo;
+            human_detail.wife_photo = wife_detail.rows[0].photo;
+            console.log("successfully query human details!");
+            res.render('marry_register', {
+              human_detail: human_detail,
+              regist_num: req.query.regist_num
+            });
+          });
+        });
+      });
     }
-    res.render('marry_register');
+    else {
+      res.render('marry_register', {
+        human_detail: human_detail,
+        regist_num: null
+      });
+    }
   } else {
     res.render('need_login');
   }
@@ -19,15 +52,25 @@ router.get('/', function (req, res, next) {
 
 //post
 router.post('/', function (req, res, next) {
+
   var marry = require('./myhfc/myhfcInvoke');
   var request = {
     chaincodeId: options.chaincode_id,
     fcn: 'marry',
-    args: [regist_num, "1", date],
+    args: [req.body.regist_num, req.body.flag, req.body.date],
     chainId: options.channel_id
   };
   marry(request, function (str) {
-    res.send(JSON.parse(str));
+    console.log(str);
+    if (str.Reason) {
+      res.send(str.Reason);
+    }
+    else {
+      var query_checked = "update marry_check set if_managed = 1, if_look = 1 WHERE check_id = \"" + req.body.regist_num + "\"";
+      mysql.executeQuery(query_checked, function (status, result) {
+
+      })
+    }
   });
 
 });
