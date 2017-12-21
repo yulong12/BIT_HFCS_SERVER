02a6fad576fb04a5141833b827af7f3cac9ecca0
wxlrainGit <wxl_rain@126.com>
2017-12-21 22:36:57 +0800

    modified child_register.html & child_register.js w-

--- routes/child_register.js
+++ routes/child_register.js
@@ -26,34 +26,17 @@ router.post('/', function (req, res, next) {
   var mother_ID = req.body.mother_ID;
   var father_ID = req.body.father_ID;
   var hospital = req.body.hospital;
-  var mother_name = req.body.mother_name;
-  var mother_age = req.body.mother_age;
-  var father_name = req.body.father_name;
-  var father_age = req.body.father_age;
   var child_name = req.body.child_name;
   var child_weight = req.body.weight;
   var child_health = req.body.health;
+  var child_place = req.body.place;
+  console.log("-----get info----");
 
-  var mother_insert = "insert into mother_table(mother_id,mother_name,mother_age)values(" + "'" + mother_ID + "','" + mother_name + "'," + mother_age + ");";
-  mysql.executeQuery(mother_insert, function () {
-
-  });
-  var father_insert = "insert into father_table(father_id,father_name,father_age)values('" + father_ID + "','" + father_name + "'," + father_age + ");";
-  var new_insert = "insert into new_table(new_id,new_name,new_weight,new_health)values(" + "'11111','" + child_name + "'," + child_weight + ",'" + child_health + "');";
-  mysql.executeQuery(father_insert, function () {
-
-  });
-  mysql.executeQuery(new_insert, function () {
-
-  });
-  console.log("mother insert--------" + mother_insert);
-  console.log("father_insert-----" + father_insert);
-  console.log("new_insert-----" + new_insert);
 
   var request = {
     chaincodeId: options.chaincode_id,
     fcn: 'createBirth',
-    args: [father_ID, mother_ID, child_sex, child_date, hospital],
+    args: [father_ID, mother_ID, child_sex, child_date, hospital,child_place,child_weight,child_health],
     chainId: options.channel_id
   };
 
